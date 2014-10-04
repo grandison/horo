81487eefaa206322dea982a6e83c6e2fe425b679
Alex <grandison@yandex.ru>
2014-10-04 03:54:06 +0200

    Dates

--- site.rb
+++ site.rb
@@ -7,6 +7,40 @@ require 'russian'
 set :protection, :except => :frame_options
 set :partial_template_engine, :erb
 
+def full_name(znak)
+	{
+		"vodoley" => "Водолей",
+		"ribi" => "Рыбы",
+		"oven" => "Овен",
+		"telec" => "Телец",
+		"blizneci" => "Близнецы",
+		"rak" => "Рак",
+		"lev" => "Лев",
+		"deva" => "Дева",
+		"vesi" => "Весы",
+		"skorpion" => "Скорпион",
+		"strelec" => "Стрелец",
+		"kozerog" => "Козерог",
+	}[znak]
+end
+
+def znak_dates(znak)
+		{
+		"vodoley" => "20 января - 19 февраля",
+		"ribi" => "19 февраля - 20 марта",
+		"oven" => "21 марта - 19 апреля",
+		"telec" => "20 апреля - 20 мая",
+		"blizneci" => "21 мая - 20 июня",
+		"rak" => "21 июня - 22 июля",
+		"lev" => "23 июля - 22 августа",
+		"deva" => "23 августа - 22 сентября",
+		"vesi" => "23 сентября - 22 октября",
+		"skorpion" => "23 октября - 21 ноября",
+		"strelec" => "22 ноября - 21 декабря",
+		"kozerog" => "22 декабря - 19 января",
+	}[znak]
+end
+
 class Cache
 	def self.get(key)
 		@caches ||= {}
